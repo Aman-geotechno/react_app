217479e8ce610f45e8143db2afb6fe3984894f15
Sanjeev <sanjeev@Sudhanshus-MacBook-Pro.local>
2023-11-08 11:51:01 +0530

    feat: fix position of header and chatbot button

--- src/components/imgButton/ImgBtn.tsx
+++ src/components/imgButton/ImgBtn.tsx
@@ -2,11 +2,11 @@ import './imgBtn.css';
 function ImgBtn(props:any){
     const handleImgBtnClick=props.handleImgBtnClick;
     const imgSrc=props.imgSrc;
+    const id=props.id;
 
 return(
-    <div className="imgBtnContainer">
-        <img className='btnImg'src={imgSrc} alt='imgBtn' onClick={()=>handleImgBtnClick()}/>
-
+    <div id={id} className="imgBtnContainer">
+        <img className='btnImg' src={imgSrc} alt='imgBtn' onClick={()=>handleImgBtnClick()}/>
     </div>
 )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { askDidiLogo } from "../../assets/images";
 import { Header, ImgBtn } from "../../components";
 import './home.css';
+import Chat from "../chat/Chat";
 
 function Home(){
     const [isChatbotButtonClicked, setIsChatbotButtonClicked]=useState(false);
@@ -14,6 +15,8 @@ return(
     <div className="home-page">
         <Header />
         <ImgBtn id='chatbotButton' imgSrc={askDidiLogo} handleImgBtnClick={handleAskDidiBtn}/>
+        {isChatbotButtonClicked && <Chat/>}
+        
     </div>
 )
 }
