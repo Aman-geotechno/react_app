d3aa203abbbfd509d713b79e2d9df16995a82b11
Sanjeev <sanjeev@Sudhanshus-MacBook-Pro.local>
2023-11-24 16:32:29 +0530

    Implement auto scroll

--- src/components/queryAns/QueryAns.jsx
+++ src/components/queryAns/QueryAns.jsx
@@ -1,3 +1,4 @@
+import { useEffect,useRef } from "react";
 import { useAppSelector } from "../../app/hooks";
 import { botIcon, speakerIcon, userIcon } from "../../assets/icons";
 import { askDidiLogo} from "../../assets/images";
@@ -7,13 +8,19 @@ function QueryAns(props) {
   //Get query and ans from store
   const query = useAppSelector((state) => state.queryAnsReducer.query);
   const answer = useAppSelector((state) => state.queryAnsReducer.ans);
+  const queryAnsWindowRef=useRef(null);
+  
+  //Auto scroll to latest msg
+  useEffect(()=>{
+    queryAnsWindowRef.current.scrollTop=queryAnsWindowRef.current.scrollHeight;
+  },[query,answer]);
 
   return (
-    <div id={props.id}>
+    <div id={props.id} ref={queryAnsWindowRef}>
       {/* Fetch and display query and answer in conversation window */}
       {query.map((question, index) => {
         return (
-          <div className="queryAns-container">        
+          <div className="queryAns-container" >        
             <div className="query" key={index}>
               <div className="query-conversation-txt-container">
                 <img className="speaker" src={speakerIcon} alt={speakerIcon} />
